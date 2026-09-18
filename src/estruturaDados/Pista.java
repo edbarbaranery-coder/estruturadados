d561e0d40d317263dcf7a5f4e75c7e1adccee31f
Francis521185 <francisnei.borges@ucsal.edu.br>
2026-09-18 12:27:43 -0300

    Atributos criados
    foi criado atributos da Class Pista

--- src/estruturaDados/Pista.java
+++ src/estruturaDados/Pista.java
@@ -2,4 +2,41 @@ package estruturaDados;
 
 public class Pista {
 
+	// Atributos
+	private String nomeIlha;
+	private String mensagem;
+	
+	// Atributos tipo "NO"
+	private Pista proximaPixta;
+	
+	// Construtor
+	public String getNomeIlha() {
+		return nomeIlha;
+	}
+
+	public void setNomeIlha(String nomeIlha) {
+		this.nomeIlha = nomeIlha;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+
+	public Pista getProximaPixta() {
+		return proximaPixta;
+	}
+
+	public void setProximaPixta(Pista proximaPixta) {
+		this.proximaPixta = proximaPixta;
+	}
+
+	public Pist(String nomeIlha, String mensagem) {
+		this.nomeIlha = nomeIlha;
+		this.mensagem = mensagem;
+		this.proximaPixta = null
+	}
 }
