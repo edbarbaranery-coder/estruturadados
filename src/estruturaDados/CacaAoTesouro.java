4eab2b79377870563fcc13bfa9427526426960af
Francis521185 <francisnei.borges@ucsal.edu.br>
2026-09-18 13:09:50 -0300

    Atributos
    Foi  construido os atributos da class Caça Ao Tesouro

--- src/estruturaDados/CacaAoTesouro.java
+++ src/estruturaDados/CacaAoTesouro.java
@@ -1,5 +1,13 @@
 package estruturaDados;
 
 public class CacaAoTesouro {
-
+	
+	//Ponteiro para o inicio da Lista (Head)
+	private Pista primeiraPista;
+	//Ponteiro para  o fim da Lista (Tail)
+	private Pista ultimaPista;
+	
+	// Construtor para iniciar lista vazia
+	
+	
 }
